0a3e7dccbc29a68ebdeaa3ab0de272d7354d2db1
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-06-27 21:53:22 +0430

    create layout for calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Keypad  from './layout/keypad/Keypad';
+import Screen from './layout/screen/Screen';
 
 function App() {
   return (
     <div className="App">
+      <Screen/>
+      <Keypad/>
     </div>
   );
 }
---
+++ calculator/src/layout/screen/header/header.js
@@ -0,0 +1,10 @@
+import classes from  "./header.module.css"
+function Header() {
+    return (
+        <div className={classes.header}>
+            header
+        </div>
+    )
+}
+
+export { Header }
