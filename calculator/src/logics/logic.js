3f547aa5ec07b6355cf75790ba653487c4fb5ffe
mohammad kalantar <kalantari01@gmail.com>
2021-06-27 20:08:08 +0430

    primary operators implemented

---
+++ calculator/src/logics/logic.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import Button from '../components/Button'
+
+class Logic extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            result : ""
+        }
+    }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+    calculate = () => {
+        var checkResult = ''
+        if(this.state.result.includes('--')){
+            checkResult = this.state.result.replace('--','+')
+        }
+
+        else {
+            checkResult = this.state.result
+        }
+
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(checkResult) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        return(
+            <>
+                {/*<Result result={this.state.result} />*/}
+                <Button onClick={this.onClick} />
+            </>
+        )
+    }
+
+
+}
+
