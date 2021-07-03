c46f69f278b5a2e15517d3d831ec874b149f9a44
rasoul <rasoulkordestani12@gmail.com>
2021-07-03 16:07:54 +0430

    stabling 3 tasks ( fist step )

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-<<<<<<< HEAD
-// import reportWebVitals from "./reportWebVitals";
-=======
->>>>>>> origin/Feature/UI
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
